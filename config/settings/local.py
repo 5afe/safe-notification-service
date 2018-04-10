9536a1bbea15d9e9a6fbbb0946ca36ec66aa9af8
Giacomo Licari <giacomo.licari@gmail.com>
2018-04-10 16:36:32 +0200

    Updated djangorestframework-camel-case to 1.0b1, added auth creation view and serializer

--- config/settings/local.py
+++ config/settings/local.py
@@ -27,14 +27,6 @@ CACHES = {
     }
 }
 
-# DATABASES
-# ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#databases
-DATABASES = {
-    'default': env.db('DATABASE_URL'),
-}
-DATABASES['default']['ATOMIC_REQUESTS'] = True
-
 # TEMPLATES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#templates
--- gnosis_safe_push_service/safe/serializers.py
+++ gnosis_safe_push_service/safe/serializers.py
@@ -1,2 +1,35 @@
+# -*- coding: utf-8 -*-
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from .models import Device
+import logging
 
 
+infoLogger = logging.getLogger('infoLogger')
+errorLogger = logging.getLogger('errorLogger')
+
+
+class AuthSerializer(serializers.Serializer):
+    """
+
+    """
+    push_token = serializers.CharField()
+    signature = serializers.CharField()
+
+    def validate_signature(self, value):
+        # TODO validation
+        # raise ValidationError(['Bad signature'])
+        return value
+
+    def get_owner(self, signature):
+        # TODO include signature recovery
+        return '0x' # TODO change
+
+    def create(self, validated_data):
+        owner = self.get_owner(validated_data.get('signature'))
+
+        instance, created = Device.objects.update_or_create(
+            push_token=validated_data.get('push_token'),
+            owner=owner
+        )
+        return instance
