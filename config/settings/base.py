83da613a69f8b1d6916bed2de28fcd32668b379c
Uxio Fuentefria <uxio@gnosis.pm>
2018-04-12 11:20:44 +0200

    Fix nginx static files

--- config/settings/base.py
+++ config/settings/base.py
@@ -89,7 +89,7 @@ MIDDLEWARE = [
 # STATIC
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = str(ROOT_DIR('staticfiles'))
+STATIC_ROOT = env('STATIC_ROOT', default=str(ROOT_DIR('staticfiles')))
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
