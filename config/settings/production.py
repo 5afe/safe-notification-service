2056892f4165d5465c50c5199f414dcd4a20afd7
Uxio Fuentefria <uxio@gnosis.pm>
2018-04-13 17:11:41 +0200

    Fix default DJANGO_ADMIN_URL

--- config/settings/production.py
+++ config/settings/production.py
@@ -71,7 +71,7 @@ EMAIL_SUBJECT_PREFIX = env('DJANGO_EMAIL_SUBJECT_PREFIX', default='[Gnosis Safe 
 # ADMIN
 # ------------------------------------------------------------------------------
 # Django Admin URL regex.
-ADMIN_URL = env('DJANGO_ADMIN_URL')
+ADMIN_URL = env('DJANGO_ADMIN_URL', default=r'^admin/')
 
 # Gunicorn
 # ------------------------------------------------------------------------------
