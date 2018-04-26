3dd34a7b33c553623c25fcdd1fea34eeea6dcc14
Uxio Fuentefria <uxio@gnosis.pm>
2018-04-26 19:17:32 +0200

    Use cryptography instead of pycrypto

--- safe_push_service/safe/helpers.py
+++ safe_push_service/safe/helpers.py
@@ -1,13 +1,21 @@
 import base64
 
-from Crypto.Hash import SHA
-from Crypto.PublicKey import RSA
-from Crypto.Signature import PKCS1_v1_5
+from cryptography.exceptions import InvalidSignature
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import padding
 
 
 def validate_google_billing_purchase(public_base64_key: str, signed_data: str, signature: str) -> bool:
-    public_key = RSA.importKey(base64.standard_b64decode(public_base64_key))
-    verifier = PKCS1_v1_5.new(public_key)
-    data_hash = SHA.new(signed_data.encode())
-    decoded_signature = base64.b64decode(signature)
-    return verifier.verify(data_hash, decoded_signature)
+    public_key = serialization.load_der_public_key(
+            base64.b64decode(public_base64_key), backend=default_backend()
+        )
+    # note the signature is base64 encoded
+    signature = base64.b64decode(signature.encode())
+    # as per https://developer.android.com/google/play/billing/billing_reference.html
+    # the signature uses "the RSASSA-PKCS1-v1_5 scheme"
+    try:
+        public_key.verify(signature, signed_data.encode(), padding.PKCS1v15(), hashes.SHA1())
+        return True
+    except InvalidSignature:
+        return False
