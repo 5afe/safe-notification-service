9df88f8aefc35162e922172f19d3feed00e6a8ca
Uxio Fuentefria <uxio@gnosis.pm>
2020-01-15 14:07:39 +0100

    Return 400 on 4Kb message limit exceeded
    - Refactor tests
    - Closes #66

--- safe_notification_service/safe/tests/test_views.py
+++ safe_notification_service/safe/tests/test_views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.urls import reverse
 
 from eth_account import Account
@@ -49,20 +51,20 @@ class TestViews(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_pairing_creation(self):
-        another_device_address, another_device_key = get_eth_address_with_key()
-        device_address, device_key = get_eth_address_with_key()
-        data = get_pairing_mock_data(another_device_address=another_device_address,
-                                     another_device_key=another_device_key, device_key=device_key)
+        another_device_account = Account.create()
+        device_account = Account.create()
+        data = get_pairing_mock_data(another_device_account=another_device_account,
+                                     device_account=device_account)
 
-        DeviceFactory(owner=another_device_address)
-        DeviceFactory(owner=device_address)
+        DeviceFactory(owner=another_device_account.address)
+        DeviceFactory(owner=device_account.address)
 
         # Repeat same request (make sure creation is idempotent)
         for _ in range(0, 2):
             response = self.client.post(reverse('v1:pairing'), data=data, format='json')
             self.assertEqual(response.status_code, status.HTTP_201_CREATED)
             response_json = response.json()
-            self.assertEqual(set(response_json['devicePair']), {another_device_address, device_address})
+            self.assertEqual(set(response_json['devicePair']), {another_device_account.address, device_account.address})
             self.assertEqual(DevicePair.objects.count(), 2)
 
     def test_pairing_creation_without_auth(self):
@@ -70,48 +72,48 @@ class TestViews(APITestCase):
         Test pairing with devices that haven't been authenticated before
         """
 
-        another_device_address, another_device_key = get_eth_address_with_key()
-        device_address, device_key = get_eth_address_with_key()
-        data = get_pairing_mock_data(another_device_address=another_device_address,
-                                     another_device_key=another_device_key, device_key=device_key)
+        another_device_account = Account.create()
+        device_account = Account.create()
+        data = get_pairing_mock_data(another_device_account=another_device_account,
+                                     device_account=device_account)
 
         response = self.client.post(reverse('v1:pairing'), data=data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         self.assertEqual(DevicePair.objects.count(), 2)
 
-        self.assertIsNone(Device.objects.get(owner=another_device_address).push_token)
-        self.assertIsNone(Device.objects.get(owner=device_address).push_token)
+        self.assertIsNone(Device.objects.get(owner=another_device_account.address).push_token)
+        self.assertIsNone(Device.objects.get(owner=device_account.address).push_token)
 
     def test_pairing_deletion(self):
-        another_device = Account.create()
-        device = Account.create()
-        device_2 = Account.create()
+        another_device_account = Account.create()
+        device_account = Account.create()
+        device_2_account = Account.create()
 
-        pairing_data = get_pairing_mock_data(another_device_address=another_device.address,
-                                             another_device_key=another_device.key, device_key=device.key)
+        pairing_data = get_pairing_mock_data(another_device_account=another_device_account,
+                                             device_account=device_account)
 
-        pairing_data_2 = get_pairing_mock_data(another_device_address=device_2.address,
-                                               another_device_key=device_2.key, device_key=device.key)
+        pairing_data_2 = get_pairing_mock_data(another_device_account=device_2_account,
+                                               device_account=device_account)
 
-        DeviceFactory(owner=device.address)
-        DeviceFactory(owner=another_device.address)
-        DeviceFactory(owner=device_2.address)
+        DeviceFactory(owner=device_account.address)
+        DeviceFactory(owner=another_device_account.address)
+        DeviceFactory(owner=device_2_account.address)
 
         for pairing in (pairing_data, pairing_data_2):
             response = self.client.post(reverse('v1:pairing'), data=pairing, format='json')
             self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         deletion_data = {
-            'device': another_device.address,
-            'signature': get_signature_json(another_device.address, device.key)
+            'device': another_device_account.address,
+            'signature': get_signature_json(another_device_account.address, device_account.key)
         }
 
         response = self.client.delete(reverse('v1:pairing'), data=deletion_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-        self.assertEqual(DevicePair.objects.filter(authorizing_device__owner=device.address).count(), 1)
-        self.assertEqual(DevicePair.objects.filter(authorized_device__owner=device.address).count(), 1)
+        self.assertEqual(DevicePair.objects.filter(authorizing_device__owner=device_account.address).count(), 1)
+        self.assertEqual(DevicePair.objects.filter(authorized_device__owner=device_account.address).count(), 1)
 
     def test_notification_creation(self):
         data = get_notification_mock_data()
@@ -119,19 +121,24 @@ class TestViews(APITestCase):
         request = self.client.post(reverse('v1:notifications'), data=data, format='json')
         self.assertEqual(request.status_code, status.HTTP_404_NOT_FOUND)
 
-        another_device_address, _ = get_eth_address_with_key()
-        device_address, device_key = get_eth_address_with_key()
-        d1 = DeviceFactory(owner=another_device_address)
-        d2 = DeviceFactory(owner=device_address)
+        another_device_account = Account.create()
+        account = Account.create()
+        d1 = DeviceFactory(owner=another_device_account.address)
+        d2 = DeviceFactory(owner=account.address)
         DevicePairFactory(authorizing_device=d1, authorized_device=d2)
         DevicePairFactory(authorizing_device=d2, authorized_device=d1)
 
-        data = get_notification_mock_data(devices=[another_device_address],
-                                          eth_address_and_key=(device_address, device_key))
-
+        data = get_notification_mock_data(devices=[another_device_account.address], account=account)
         request = self.client.post(reverse('v1:notifications'), data=data, format='json')
-
         self.assertEqual(request.status_code, status.HTTP_204_NO_CONTENT)
+
+        # Test 4Kb message limit
+        data = get_notification_mock_data(message=json.dumps({'my_message': 'A' * 4096}),
+                                          devices=[another_device_account.address],
+                                          account=account)
+        request = self.client.post(reverse('v1:notifications'), data=data, format='json')
+        self.assertEqual(request.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(request.json()['message'][0], 'Ensure this field has no more than 4096 characters.')
 
     def test_simple_notification_creation(self):
         random_address, _ = get_eth_address_with_key()
